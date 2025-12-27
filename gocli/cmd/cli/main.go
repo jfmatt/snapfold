9d0c33376d0fa3d8369cf9cf768aae831969a6df
James Matthews <github@jmatthews.dev>
2025-12-26 20:04:06 -0800

    build with protos working

--- gocli/cmd/cli/main.go
+++ gocli/cmd/cli/main.go
@@ -6,31 +6,35 @@ import (
 
 	"github.com/jfmatt/snapfold/lib/greeting"
 	"github.com/spf13/cobra"
+
+	pb "github.com/jfmatt/snapfold/gamedef"
 )
 
-var rootCmd = &cobra.Command{
-	Use:   "gocli",
-	Short: "A simple CLI tool built with Cobra",
-	Long:  `A demonstration Go binary that uses the Cobra library and a shared library module.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Hello from the Go CLI!")
-		fmt.Println("Built with Cobra library")
-		// Use the shared library
-		fmt.Println(greeting.GetGreeting(""))
-		if len(args) > 0 {
-			fmt.Printf("Arguments: %v\n", args)
-			fmt.Printf("args: %v", args)
-		}
-	},
-}
+func rootCmd() *cobra.Command {
+	c := &cobra.Command{
+		Use:   "gocli",
+		Short: "A simple CLI tool built with Cobra",
+		Long:  `A demonstration Go binary that uses the Cobra library and a shared library module.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello from the Go CLI!")
+			fmt.Println("Built with Cobra library")
+			// Use the shared library
+			fmt.Println(greeting.GetGreeting(""))
+			if len(args) > 0 {
+				fmt.Printf("Arguments: %v\n", args)
+			}
 
-func init() {
-	// Add the greet command from the shared library
-	rootCmd.AddCommand(greeting.NewGreetCommand())
+			protoVal := &pb.TableConfig{}
+			fmt.Printf("proto (%T): %s\n", protoVal, protoVal.String())
+		},
+	}
+	c.AddCommand(greeting.NewGreetCommand())
+
+	return c
 }
 
 func main() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := rootCmd().Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
